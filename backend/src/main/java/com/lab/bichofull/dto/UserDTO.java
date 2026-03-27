3a925b700b6f465f50fd461bcd861dfcdf431579
Larissa Monsalve <larissamonsalve14@gmail.com>
2026-03-27 02:29:52 -0400

    fix: adicionando dependencias faltantes no pom.xml e corrigindo DTO

--- backend/src/main/java/com/lab/bichofull/dto/UserDTO.java
+++ backend/src/main/java/com/lab/bichofull/dto/UserDTO.java
@@ -2,9 +2,13 @@ package com.lab.bichofull.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
     private String name;
     private String email;
